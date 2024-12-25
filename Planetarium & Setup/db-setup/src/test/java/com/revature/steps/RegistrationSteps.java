e210a3b0b270cf7579d20849acd1e2aec5d78a7a
Jonathan Roman <jonroman2525@gmail.com>
2024-12-25 13:43:09 -0700

    adding step functions for registration feature

---
+++ Planetarium & Setup/db-setup/src/test/java/com/revature/steps/RegistrationSteps.java
@@ -0,0 +1,56 @@
+package com.revature.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegistrationSteps {
+
+    @Given("the new user is on the login page")
+    public void the_new_user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks the register link")
+    public void the_user_clicks_the_register_link() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user provides a {string}")
+    public void the_user_provides_a(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user submits valid credentials")
+    public void the_user_submits_valid_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should get a browser alert saying {string}")
+    public void the_user_should_get_a_browser_alert_saying(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("should be redirected to the login page.")
+    public void should_be_redirected_to_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the user is on the registration page")
+    public void the_user_is_on_the_registration_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on the login link")
+    public void the_user_clicks_on_the_login_link() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- Planetarium & Setup/db-setup/src/test/resources/features/registration.feature
+++ Planetarium & Setup/db-setup/src/test/resources/features/registration.feature
@@ -1,6 +1,6 @@
 Feature: Planetarium User Registration Positive and negative paths
-
   Scenario Outline: User Registration
+
     Given the new user is on the login page
     When the user clicks the register link
     When the user provides a "<username>"
@@ -11,18 +11,23 @@ Feature: Planetarium User Registration Positive and negative paths
 
     Examples:
     |username|password|response|
-  |Super_man-2001|Krypton-was_2000|Account create successfully alert|
-  |Batman|Krypton-was_2000|Invalid username alert|
-  |Bane|Krypton-was_2000|Invalid username alert|
-  |wonder_woman_for_the_DC_theming|	Krypton-was_2000|Invalid username alert|
-  |2face|Krypton-was_2000|Invalid username alert|
-  |joker!!!!!!?)|Krypton-was_2000|Invalid username alert|
-  |Super_man-2001|b0Ts|Invalid password alert|
-  |Super_man-2001|AlfredIsTheBestButlerToExist111|Invalid password alert|
-  |Super_man-2001|3atman|Invalid password alert|
-  |Super_man-2001|A1fredIsTheBestButlerToExist!!|Invalid password alert|
-  |Super_man-2001|batman1|Invalid password alert|
-  |Super_man-2001|BATMAN1|Invalid password alert|
-  |Super_man-2001|Robin|Invalid password alert|
+  |Super_man-2001|Krypton-was_2000|Account create successfully|
+  |Batman|Krypton-was_2000|Invalid username|
+  |Bane|Krypton-was_2000|Invalid username|
+  |wonder_woman_for_the_DC_theming|	Krypton-was_2000|Invalid username |
+  |2face|Krypton-was_2000|Invalid username|
+  |joker!!!!!!?)|Krypton-was_2000|Invalid username|
+  |Super_man-2001|b0Ts|Invalid password|
+  |Super_man-2001|AlfredIsTheBestButlerToExist111|Invalid password|
+  |Super_man-2001|3atman|Invalid password|
+  |Super_man-2001|A1fredIsTheBestButlerToExist!!|Invalid password|
+  |Super_man-2001|batman1|Invalid password|
+  |Super_man-2001|BATMAN1|Invalid password|
+  |Super_man-2001|Robin|Invalid password|
+
+    Scenario: Returning to login page
+      Given the user is on the registration page
+      When the user clicks on the login link
+      Then the user should be redirected to login page.
 
 
